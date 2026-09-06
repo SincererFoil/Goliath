304201464feb1ab1f62b3789e1fdd3bf02ebaafa
SincererFoil <sincererfoilyt@gmail.com>
2026-09-06 02:13:45 +0200

    feat: Changed History save amount to 499

--- src/main/java/ch/mcserver/goliath/database/mongodb/repository/HistoryEventRepository.java
+++ src/main/java/ch/mcserver/goliath/database/mongodb/repository/HistoryEventRepository.java
@@ -19,7 +19,7 @@ public class HistoryEventRepository {
 
     public void createEvent(UUID uuid, String type, String title, String server, String historyId) {
 
-        if (collection.countDocuments(Filters.eq("uuid", uuid.toString())) > 119) {
+        if (collection.countDocuments(Filters.eq("uuid", uuid.toString())) > 499) {
             List<ObjectId> ids = collection.find(Filters.eq("uuid", uuid.toString()))
                     .sort(Sorts.ascending("createdAt"))
                     .limit(1)
